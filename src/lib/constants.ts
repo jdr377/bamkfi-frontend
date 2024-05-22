e2b047ca1bfac0c6a464a457e5cad54d392bc006
remjx <14482423-remjx@users.noreply.gitlab.com>
2024-05-22 11:27:40 -0400

    fix: points per block

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,4 @@ export const TELEGRAM_URL = 'https://t.me/bamkfi'
 export const BAMK_MARKET_URL = 'https://magiceden.io/runes/BAMK%E2%80%A2OF%E2%80%A2NAKAMOTO%E2%80%A2DOLLAR'
 export const NUSD_MARKET_URL = 'https://unisat.io/market/brc20?tick=%24NUSD'
 export const GENESIS_POINTS_BLOCK = 844424; // https://ordinals.com/inscription/67aec086c39b89ab26eec6c77b9055c58adc1395a2536c98c4cbd7871c207ccfi0
-export const POINTS_PER_BLOCK = 50_000;
+export const POINTS_PER_BLOCK = 31_250;
