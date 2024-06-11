dfcf6caa36891022cfb336cb23d371f8532846b7
John Calhoun <jcalhoun@trifinlabs.com>
2024-06-11 13:31:42 -0400

    checkpoint

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { Nunito } from 'next/font/google';
 import classNames from 'classnames';
 import { FaCopy } from 'react-icons/fa';
+import styles from './leaderboard.module.css'
 
 const nunito = Nunito({ subsets: ['latin'] });
 
@@ -25,7 +26,7 @@ async function getRewardsByAddress(address: string) {
 
 function shortenAddress(address?: string): string {
   if (!address || address.length <= 10) return address || '';
-  return `${address.slice(0, 5)}...${address.slice(-4)}`;
+  return `${address.slice(0, 4)}...${address.slice(-4)}`;
 }
 
 function copyToClipboard(text: string) { //handles pc, android but not tested for apple devices
@@ -69,7 +70,7 @@ export default function Leaderboard() {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsNarrowScreen(window.innerWidth < 700);
+      setIsNarrowScreen(window.innerWidth < 953);
     };
 
     // Initial check
@@ -111,88 +112,97 @@ export default function Leaderboard() {
 
   return (
 	<div className="max-w-screen-xl container flex flex-col gap-8 sm:mt-8">
-	  <div className="flex flex-col gap-4 mx-3 md:mx-8">
+		<div className="flex flex-col gap-4 mx-3 md:mx-8">
 		<h1 className={classNames(nunito.className, 'text-3xl mt-2')}>
-		  Season 1 Airdrop Leaderboard
+			Season 1 Airdrop Leaderboard
 		</h1>
 		<div>
-		  NUSD Rune and BRC-20 holders accrue pro-rata rewards of 31,250 BAMK per block.
-		  <br/>
-		  Rewards will be released 41,982 blocks after the reward is accrued.
+			NUSD Rune and BRC-20 holders accrue pro-rata rewards of 31,250 BAMK per block.
+			<br/>
+			Rewards will be released 41,982 blocks after the reward is accrued.
 		</div>
-	  </div>
-	  <div className="max-w-screen-xl mx-3 md:mx-8">
+		</div>
+		<div className="max-w-screen-xl mx-3 md:mx-8">
 		<input
-		  type="text"
-		  placeholder="Search by address"
-		  className="w-full p-2 border rounded-md"
-		  value={searchTerm}
-		  onChange={(e) => setSearchTerm(e.target.value)}
+			type="text"
+			placeholder="Search by address"
+			className="w-full p-2 border rounded-md mb-2"
+			value={searchTerm}
+			onChange={(e) => setSearchTerm(e.target.value)}
 		/>
 		<div className="relative overflow-x-auto shadow-md rounded-lg mt-0.5">
-		  <table className="w-full text-sm text-left rtl:text-right text-zinc-400">
+			<table className="w-full text-sm text-left rtl:text-right text-zinc-400">
 			<thead className="text-xs uppercase bg-zinc-700 text-zinc-400">
-			  <tr>
-				<th scope="col" className="px-2 py-3" style={{ width: '4em', maxWidth: '4em' }}>
-				  Rank
+				<tr>
+				<th scope="col" className="pl-1 py-4 whitespace-nowrap text-center pr-1" style={{  }}>
+					Rank
 				</th>
-				<th scope="col" className="px-3 py-3 text-center" style={{ width: '8em' }}>
-				  Address
+				<th scope="col" className="pl-2 py-4 whitespace-nowrap" style={{  }}>
+					Address
 				</th>
 				<th scope="col" className="px-3 py-3 text-center">
-				  Amount&nbsp;🏦
+					Amount&nbsp;🏦
 				</th>
 				<th scope="col" className="px-2 py-3 text-center">
-				  Value
+					Value
 				</th>
-			  </tr>
+				</tr>
 			</thead>
 			<tbody>
-			  {filteredResults.length > 0 ? (
+				{filteredResults.length > 0 ? (
 				<>
-				  {filteredResults.sort((a: any, b: any) => b.amount - a.amount)?.map((address: any, index: number) => (
+					{filteredResults.sort((a: any, b: any) => b.amount - a.amount)?.map((address: any, index: number) => (
 					<tr key={address.address} className="border-b bg-zinc-800 border-zinc-700 hover:bg-zinc-600 font-mono">
-					  <td scope="row" className="pl-1 py-4 whitespace-nowrap text-center">
+						<td scope="row" className="pl-1 py-4 whitespace-nowrap text-center pr-1">
 						{index + 1}
-					  </td>
-					  <td scope="row" className="pl-1 py-4 whitespace-nowrap flex items-center">
+						</td>
+						<td scope="row" className="pl-2 py-4 whitespace-nowrap flex items-center">
 						{isNarrowScreen ? shortenAddress(address.address) : address.address}
 						<FaCopy
-						  className="ml-0.5 cursor-pointer"
-						  onClick={() => copyToClipboard(address.address)}
-						  title="Copy Address"
+							className="ml-0.5 cursor-pointer"
+							onClick={() => copyToClipboard(address.address)}
+							title="Copy Address"
 						/>
-					  </td>
-					  <td className="px-1.5 py-4 text-center">
+						</td>
+						<td className="px-1.5 py-4 text-center">
 						{address.amount?.toLocaleString(undefined, { maximumFractionDigits: 0 })}
-					  </td>
-					  <td className="px-1.5 py-4 text-center">
+						</td>
+						<td className="px-1.5 py-4 text-center">
 						{data?.btcPriceData?.bitcoin.usd && address.amount && data.bamkRuneData.curPrice
-						  ? `$${((address.amount * data.bamkRuneData.curPrice) / 100000000 * data?.btcPriceData?.bitcoin.usd).toLocaleString(undefined, { maximumFractionDigits: 0 })}`
-						  : '-'}
-					  </td>
+							? `$${((address.amount * data.bamkRuneData.curPrice) / 100000000 * data?.btcPriceData?.bitcoin.usd).toLocaleString(undefined, { maximumFractionDigits: 0 })}`
+							: '-'}
+						</td>
 					</tr>
-				  ))}
+					))}
 				</>
-			  ) : (
+				) : (
 				<tr className="bg-zinc-800 hover:bg-zinc-600">
-				  <td scope="row" className="px-6 py-4 text-white">
+					<td scope="row" className="px-6 py-4 text-white">
 					Fetching leaderboard..
-				  </td>
-				  <td></td>
-				  <td></td>
-				  <td></td>
+					</td>
+					<td></td>
+					<td></td>
+					<td></td>
 				</tr>
-			  )}
+				)}
 			</tbody>
-		  </table>
+			</table>
 		</div>
-	  </div>
-	  {data.leaderboard_data?.block && (
-		<div className="ml-auto mr-auto text-zinc-400">
-		  Leaderboard synced to block {data.leaderboard_data.block}
 		</div>
-	  )}
+		{data.leaderboard_data?.block ? 
+			<div className="flex fixed right-4 bottom-4 z-10 gap-4">
+				<div className="flex flex-col justify-end bg-[#3c3333] py-2 px-4">
+					<div className="relative flex gap-x-3 items-center justify-end text-[12px]">
+						<a className="hover:underline" target="_blank" href={"https://mempool.space/block/" + data.leaderboard_data.block}>
+							{data.leaderboard_data.block}
+						</a>
+						<div className="relative h-6 w-6 flex justify-center items-center">
+							<span className="flex w-2 h-2 me-3 bg-green-500 rounded-full"></span>
+						</div>
+					</div>
+				</div>
+			</div>
+		: [] }
 	</div>
   );  
 }
