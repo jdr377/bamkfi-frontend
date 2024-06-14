caa2d824422ce7040734c76e39435819d6874e99
flopasen <69904251+flopasen@users.noreply.github.com>
2024-06-15 01:49:03 +1000

    reorder slightly

--- src/components/externals.tsx
+++ src/components/externals.tsx
@@ -3,16 +3,7 @@ export default function Externals() {
     <div className="relative h-28 sm:h-36 mt-6">
       <div className="flex justify-center items-center h-full px-4 md:px-8">
         <div className="flex flex-wrap justify-center items-center gap-4 max-w-lg mx-auto" style={{ maxWidth: '600px' }}>
-          <a href="https://magiceden.io/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
-            <img
-              src="/logos/me.svg"
-              alt="Magic Eden"
-              width={120}
-              height={100}
-              className="grayscale group-hover:grayscale-0 transition-all duration-300"
-            />
-          </a>
-          <a href="https://www.okx.com/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+        <a href="https://www.okx.com/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/okx.png"
               alt="OKX"
@@ -21,12 +12,12 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://ordinalswallet.com/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://magiceden.io/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
-              src="/logos/ow.png"
-              alt="Ordinals Wallet"
-              width={120}
-              height={140}
+              src="/logos/me.svg"
+              alt="Magic Eden"
+              width={130}
+              height={100}
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
@@ -36,6 +27,15 @@ export default function Externals() {
               alt="Unisat"
               width={120}
               height={100}
+              className="grayscale group-hover:grayscale-0 transition-all duration-300"
+            />
+          </a>
+          <a href="https://ordinalswallet.com/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+            <img
+              src="/logos/ow.png"
+              alt="Ordinals Wallet"
+              width={130}
+              height={140}
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
