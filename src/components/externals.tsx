c68a8a44a30e05be5651c846a4ad4c5a7af92d6e
remjx <markjackson02@ymail.com>
2024-06-15 01:00:16 -0400

    link to externals bamk page where possible

--- src/components/externals.tsx
+++ src/components/externals.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
+
 export default function Externals() {
   return (
     <div className="relative h-28 sm:h-36 mt-6">
       <div className="flex justify-center items-center h-full px-4 md:px-8">
         <div className="flex flex-wrap justify-center items-center gap-4 max-w-lg mx-auto" style={{ maxWidth: '600px' }}>
-        <a href="https://www.okx.com/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+        <a href="https://www.okx.com/web3/marketplace/runes/token/BAMK%E2%80%A2OF%E2%80%A2NAKAMOTO%E2%80%A2DOLLAR/840256:35" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/okx.png"
               alt="OKX"
@@ -12,7 +14,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://magiceden.io/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://magiceden.io/runes/BAMK%E2%80%A2OF%E2%80%A2NAKAMOTO%E2%80%A2DOLLAR" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/me.svg"
               alt="Magic Eden"
@@ -21,7 +23,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://unisat.io/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://unisat.io/runes/market?tick=BAMK%E2%80%A2OF%E2%80%A2NAKAMOTO%E2%80%A2DOLLAR" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/unisat.svg"
               alt="Unisat"
@@ -30,7 +32,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://ordinalswallet.com/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://ordinalswallet.com/collection/rune-BAMK%E2%80%A2OF%E2%80%A2NAKAMOTO%E2%80%A2DOLLAR" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/ow.png"
               alt="Ordinals Wallet"
@@ -39,7 +41,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://www.saturnbtc.io/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://www.saturnbtc.io/app/swap/bamkofnakamotodollar-sat" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/saturn.svg"
               alt="Saturn"
@@ -48,7 +50,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://www.dotswap.app/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://www.dotswap.app/swap#R_BTC_BAMK%E2%80%A2OF%E2%80%A2NAKAMOTO%E2%80%A2DOLLAR" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/dotswap.png"
               alt="Dotswap"
@@ -57,7 +59,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://liquidium.fi/" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://app.liquidium.fi/lend/runes" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/liquidium.svg"
               alt="Liquidium"
@@ -66,7 +68,7 @@ export default function Externals() {
               className="grayscale group-hover:grayscale-0 transition-all duration-300"
             />
           </a>
-          <a href="https://fluidtokens.com//" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
+          <a href="https://btc.fluidtokens.com/swap" target="_blank" rel="noopener noreferrer" className="group flex-1 min-w-[120px] max-w-[33%] flex justify-center items-center">
             <img
               src="/logos/fluid.png"
               alt="FluidTokens"
