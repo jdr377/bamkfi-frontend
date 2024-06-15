f805085cd34c3f33eff8c9fb90e376e74bf4fddf
remjx <markjackson02@ymail.com>
2024-06-15 05:34:03 -0400

    fix react-fitty module declaration issue

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,13 +1,3 @@
-declare module 'react-fitty' {
-    const ReactFitty: React.ForwardRefExoticComponent<React.HTMLAttributes<HTMLDivElement> & {
-        children?: React.ReactNode;
-        minSize?: number | undefined;
-        maxSize?: number | undefined;
-        wrapText?: boolean | undefined;
-    } & React.RefAttributes<HTMLElement>>;
-    export { ReactFitty };
-}
-
 export interface UnisatBamkData {
     tick: string;
     symbol: string;
